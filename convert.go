1054fad9d5804cb15fe1ac78e408510940559876
黑夜路人 <heiyeluren@qq.com>
2022-09-06 16:11:12 +0800

    Add files via upload

---
+++ convert.go
@@ -0,0 +1,55 @@
+package common
+
+import "strconv"
+
+func ToInt(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	v, err := strconv.Atoi(s)
+	if err != nil {
+		return 0
+	}
+
+	return v
+}
+
+func ToInt32(s string) int32 {
+	if s == "" {
+		return 0
+	}
+
+	v, err := strconv.ParseInt(s, 0, 0)
+	if err != nil {
+		return 0
+	}
+
+	return int32(v)
+}
+
+func ToUint64(s string) uint64 {
+	if s == "" {
+		return 0
+	}
+
+	v, err := strconv.ParseUint(s, 0, 64)
+	if err != nil {
+		return 0
+	}
+
+	return v
+}
+
+func ToFloat32(s string) float32 {
+	if s == "" {
+		return 0
+	}
+
+	v, err := strconv.ParseFloat(s, 32)
+	if err != nil {
+		return 0
+	}
+
+	return float32(v)
+}
---
+++ functions.go
@@ -0,0 +1,17 @@
+package common
+
+import (
+	"github.com/spf13/cast"
+	"log"
+	"runtime/debug"
+)
+
+func Recover() {
+	if rec := recover(); rec != nil {
+		if err, ok := rec.(error); ok {
+			log.Printf("PanicRecover Unhandled error: %v\n stack:%v \n", err.Error(), cast.ToString(debug.Stack()))
+		} else {
+			log.Printf("PanicRecover Panic: %v\n stack:%v \n", rec, cast.ToString(debug.Stack()))
+		}
+	}
+}
