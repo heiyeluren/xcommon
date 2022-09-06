9ffcc01ecc66417c1fdbea6e08bb7594278a8007
黑夜路人 <heiyeluren@qq.com>
2022-09-06 16:15:41 +0800

    Add files via upload

---
+++ retry/retry.go
@@ -0,0 +1,27 @@
+package common
+
+import (
+	"context"
+	"time"
+)
+
+type RetryFunc func(ctx context.Context) error
+
+//如果f方法出现错误，重试retryTimes次
+func Retry(ctx context.Context, f RetryFunc, retryTimes uint8) error {
+	for {
+		if err := f(ctx); err != nil {
+			if retryTimes == 0 {
+				return err
+			}
+
+			retryTimes--
+			time.Sleep(1 * time.Millisecond)
+			continue
+		}
+
+		return nil
+	}
+
+	return nil
+}
